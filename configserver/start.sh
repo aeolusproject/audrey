c289d1b17fed16c2db887cffdb87e29a3262fc7e
Greg Blomquist <gblomqui@redhat.com>
2011-08-17 14:15:15 -0400

    updating start.sh dev script to pass through args to thin

--- configserver/start.sh
+++ configserver/start.sh
@@ -2,9 +2,9 @@
 
 export STORAGE_DIR="/tmp/audrey"
 export INSTANCE_CONFIG_RNG="../schema/instance-config.rng"
-export AEOLUS_CONFSERVER_VERSION="0.2.0"
+export AEOLUS_CONFSERVER_VERSION="0.2.3"
 
 cd src
 
 
-thin -R config.ru -p 4567 start
+thin -R config.ru -p 4567 start $@
